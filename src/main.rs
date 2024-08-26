8f0f935c7080f02b1f6ef70305a843b8a1b66fcb
LUC BILLAUD <luc.billaud@univ-lyon1.fr>
2024-08-26 17:17:28 +0200

    Added icons

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([720.0, 600.0])
-            .with_min_inner_size([600.0, 400.0]),
-            // .with_icon(
-            //     // NOTE: Adding an icon is optional
-            //     eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-            //         .expect("Failed to load icon"),
-            // ),
+            .with_min_inner_size([600.0, 400.0])
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .expect("Failed to load icon"),
+            )
+            .with_title("FindYourBF - The Game"),
         ..Default::default()
     };
     eframe::run_native(
